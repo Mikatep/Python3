a2fc0f56c540a29bf38340b7ff33e0a3c5b6b7d9
Mikatep <miriamcordobacapote@gmail.com>
2022-06-05 18:28:54 +0000

    nav added

--- app.py
+++ app.py
@@ -22,3 +22,8 @@ mongo = PyMongo(app)
 def get_recipes():
     recipes = list(mongo.db.tasks.find())
     return render_template("home.html", recipes=recipes)
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
