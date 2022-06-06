5f6409df476703f7fa39f31427401ab4dac289f7
Mikatep <miriamcordobacapote@gmail.com>
2022-06-06 15:45:51 +0000

    register page designed, register and login functionality added and working properly

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,4 @@
 $(document).ready(function(){
-    $('.sidenav').sidenav();
-    $('.collapsible').collapsible();
-    $('.tooltipped').tooltip();
     $("select").formSelect();
     $(".datepicker").datepicker({
       format: "dd mmmm, yyyy",
