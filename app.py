21bad565940e5b6c59de371ddd4afae3b20a47da
Mikatep <miriamcordobacapote@gmail.com>
2022-06-06 17:19:15 +0000

    add-recipe function created, and newrecipe.html designed

--- app.py
+++ app.py
@@ -99,6 +99,26 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/add_recipe", methods=["GET", "POST"])
+def add_recipe():
+    if request.method == "POST":
+        recipe = {
+            "category_name": request.form.get("category_name"),
+            "recipe_name": request.form.get("recipe_name"),
+            "recipe_ingredients": request.form.get("recipe_ingredients"),
+            "recipe_steps": request.form.get("recipe_steps"),
+            "recipe_cuisine": request.form.get("recipe_cuisine"),
+            "recipe_picture": request.form.get("recipe_picture"),
+            "created_by": session["user"]
+        }
+        mongo.db.recipes.insert_one(recipe)
+        flash("Recipe Sucessfully Added, thank you for your sharing!")
+        return redirect(url_for("get_recipes"))
+
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    return render_template("newrecipe.html", categories=categories)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
